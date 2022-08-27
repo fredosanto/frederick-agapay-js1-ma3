15138c199536e18f28cf69d4d27ed32b1388cc26
fredosanto <93183340+fredosanto@users.noreply.github.com>
2022-08-27 13:02:52 +0200

    Finished question-1.js

---
+++ js/question-1.js
@@ -0,0 +1,10 @@
+// function getRemainder(a,b) {
+//     return a % b;
+//     }
+
+
+const getRemainder = (a, b) => a % b;
+
+const result = getRemainder(4, 2);
+
+console.log(result);
---
+++ js/question-2.js
@@ -0,0 +1,4 @@
+const key = "58668492806240b88e75b312d2881031";
+const url = `https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=${key}`;
+
+console.log(url);
